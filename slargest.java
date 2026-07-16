435c803df76b5ef879566e342790e56a5f658ecc
Arpit Shah <shaharpit2004@gmail.com>
2026-07-16 18:19:49 +0530

    second largest using array

---
+++ slargest.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class slargest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the size of array");
+        int size=sc.nextInt();
+        int[]arr=new int[size];
+        for(int i=0;i<arr.length;i++){
+            System.out.println("Enter the elements for array:");
+            arr[i]=sc.nextInt();
+        }
+        int largest=Integer.MIN_VALUE;
+        int second_largest= Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>largest){
+                second_largest=largest;
+                largest=arr[i];
+            }
+            else if(arr[i]!=largest && largest<second_largest){
+                    second_largest=arr[i];
+            }
+        }
+        System.out.println("largest:"+largest);
+        System.out.println("second largest number"+second_largest);
+    }
+}
